418f67f15f42b379c099c71a60e2a76afc1a7117
Veena Devi <veenadevi.s@gmail.com>
2022-01-21 10:55:35 +0530

    Updated for demo

--- src/test/java/DemoPlayGroundClass.java
+++ src/test/java/DemoPlayGroundClass.java
@@ -1,6 +1,5 @@
 
-
-import org.junit.Assert;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -9,10 +8,10 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.BeforeTest;
 
-public class ZebRunnerClass {
+public class DemoPlayGroundClass {
     public RemoteWebDriver driver = null;
-//    String username = "rishabhps";
-//    String accessKey = "Qzln3pCCV7YvCYeQ7EmksXD0hSxR5STjBAK3Vh0gilJpIOllR1";
+//    String username = "";
+//    String accessKey = "";
 
     String username = System.getenv("LT_USERNAME") == null ? "" : System.getenv("LT_USERNAME");
     String authkey = System.getenv("LT_ACCESS_KEY") == null ? "" : System.getenv("LT_ACCESS_KEY");
@@ -20,15 +19,15 @@ public class ZebRunnerClass {
     @BeforeTest
     public void setUp() throws Exception {
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("build", "your build name");
-        capabilities.setCapability("name", "your test name");
+        capabilities.setCapability("build", "ZebRunner_Demo_Build");
+        capabilities.setCapability("name", "SampleRun1");
         capabilities.setCapability("platform", "Windows 10");
         capabilities.setCapability("browserName", "Chrome");
         capabilities.setCapability("version","97.0");
-       // String hubRUL="https://rishabhps:Qzln3pCCV7YvCYeQ7EmksXD0hSxR5STjBAK3Vh0gilJpIOllR1@hub.lambdatest.com/wd/hub";
+
         try {
             driver = new RemoteWebDriver(new URL("https://" + username + ":" + authkey + hub), capabilities);
-            //driver = new RemoteWebDriver(new URL("https://rishabhps:Qzln3pCCV7YvCYeQ7EmksXD0hSxR5STjBAK3Vh0gilJpIOllR1@hub.lambdatest.com/wd/hub"), capabilities);
+
         } catch (MalformedURLException e) {
             System.out.println("Incorrect grid URL");
         }
@@ -36,11 +35,14 @@ public class ZebRunnerClass {
     @Test(enabled = true)
     public void testScript() throws Exception {
         try {
-            driver.get("https://www.lambdatest.com");
-            driver.findElement(By.xpath("//button[normalize-space()='Resources']")).click();
-            driver.findElement(By.linkText("Blog")).click();
+            driver.get("https://www.lambdatest.com/selenium-playground/");
+            driver.findElement(By.linkText("Simple Form Demo")).click();
+            String userMessage="New Mesasge";
+            driver.findElement(By.id("user-message")).sendKeys(userMessage);
+            driver.findElement(By.id("showInput")).click();
+            String printedText=driver.findElement(By.id("message")).getText();
             System.out.println("blog page Title"+driver.getTitle());
-            Assert.assertTrue(true);
+            Assert.assertTrue(userMessage==(printedText));
             driver.quit();
         } catch (Exception e) {
             System.out.println(e.getMessage());
