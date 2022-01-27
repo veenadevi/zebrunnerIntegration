21848e911da0852312278f2d6ea90e4d21e97e40
Veena Devi <veenadevi.s@gmail.com>
2022-01-27 12:02:37 +0530

    Updated

---
+++ src/test/java/LTPlayGround_Checkbox_TestClass.java
@@ -0,0 +1,80 @@
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LTPlayGround_Checkbox_TestClass {
+    public RemoteWebDriver driver = null;
+
+
+    String username = System.getenv("LT_USERNAME") == null ? "" : System.getenv("LT_USERNAME");
+    String authkey = System.getenv("LT_ACCESS_KEY") == null ? "" : System.getenv("LT_ACCESS_KEY");
+    String hub = "@hub.lambdatest.com/wd/hub";
+    @BeforeTest
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("build", "ZebRunner_Demo_Build_1");
+        capabilities.setCapability("name", "LTPlayGround_SimpleForm");
+        capabilities.setCapability("platform", "Windows 11");
+    	capabilities.setCapability("browserName", "Firefox");
+    	capabilities.setCapability("version","95.0");
+    	capabilities.setCapability("console","true");
+
+        try {
+            driver = new RemoteWebDriver(new URL("https://" + username + ":" + authkey + hub), capabilities);
+            //WebDriverManager.chromedriver().setup();
+            //driver=new ChromeDriver();
+
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        }
+        catch (MalformedURLException e) {
+            System.out.println("Incorrect grid URL");
+        }
+        catch ( Exception e) {
+            System.out.println("Incorrect grid URL");
+        }
+
+    }
+
+
+    @Test(enabled = true)
+    public void testCheckBoxTest() throws Exception {
+        try {
+            Reporter.log("launching testSampleFormDemo");
+            driver.get("https://www.lambdatest.com/selenium-playground/checkbox-demo");
+           
+            File source = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+   	     	FileHandler.copy(source, new File("CheckBox LT_PlayGround"));
+   	     	
+   	     	WebElement checkBox=driver.findElement(By.id("isAgeSelected"));
+   	     	Assert.assertTrue(checkBox.isSelected());
+
+            Reporter.log("testCheckBoxTest TestCase Completed");
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+
+    @AfterMethod
+    public void teardown(){
+        driver.quit();
+
+    }
+}
---
+++ src/test/java/LTPlayGround_InputForm_TestClass.java
@@ -0,0 +1,103 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class LTPlayGround_InputForm_TestClass {
+
+
+    public RemoteWebDriver driver = null;
+
+
+    String username = System.getenv("LT_USERNAME") == null ? "" : System.getenv("LT_USERNAME");
+    String authkey = System.getenv("LT_ACCESS_KEY") == null ? "" : System.getenv("LT_ACCESS_KEY");
+    String hub = "@hub.lambdatest.com/wd/hub";
+
+
+    @BeforeTest
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("build", "ZebRunner_Demo_Build_2");
+        capabilities.setCapability("name", "LTPlayGround_InputForm");
+        capabilities.setCapability("platform", "MacOS Monterey");
+        capabilities.setCapability("browserName", "Safari");
+        capabilities.setCapability("version","15.0");
+        capabilities.setCapability("console","error");
+
+        try {
+          driver = new RemoteWebDriver(new URL("https://" + username + ":" + authkey + hub), capabilities);
+//           WebDriverManager.chromedriver().setup();
+//            driver=new ChromeDriver();
+//
+//            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        }
+//        catch (MalformedURLException e) {
+//            Assert.assertTrue(false,e.getMessage());
+//            System.out.println("Incorrect grid URL");
+//        }
+        catch ( Exception e) {
+            Assert.assertTrue(false,e.getMessage());
+           e.printStackTrace();
+        }
+
+    }
+    @Test(enabled = true)
+    public void testInputFormSubmit() throws Exception {
+
+            try {
+                Reporter.log("launching testSampleFormDemo");
+                driver.get("https://www.lambdatest.com/selenium-playground/input-form-demo");
+                driver.findElement(By.id("name")).sendKeys("LTUser");
+
+                driver.findElement(By.id("company")).sendKeys("LT");
+                
+                driver.findElement(By.id("inputEmail4")).sendKeys("LTUser@lambdatest.com");
+                
+                driver.findElement(By.id("inputPassword4")).sendKeys("1234");
+                driver.findElement(By.id("websitename")).sendKeys("https://www.lambdatest.com/");
+                Select selectCountry = new Select(driver.findElement(By.name("country")));
+                selectCountry.selectByValue("US");
+
+                driver.findElement(By.id("inputCity")).sendKeys("New York");
+
+                driver.findElement(By.id("inputState")).sendKeys("New York");
+
+
+                driver.findElement(By.id("inputAddress1")).sendKeys("StreetAddress");
+                driver.findElement(By.id("inputAddress2")).sendKeys("StreetAddress2");
+                
+                driver.findElement(By.id("inputZip")).sendKeys("100052");
+                driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+                Thread.sleep(2000);
+
+                String successMessage = driver.findElement(By.className("success-msg hidden")).getText();
+
+                System.out.println("successMessage  "+successMessage);
+                
+                Assert.assertEquals(successMessage, "Thanks for contacting us, we will get back to you shortly.", "Invalid Message");
+               
+
+                Reporter.log("testSampleFormDemo TestCase Completed");
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+    @AfterTest
+    public void teardown(){
+     driver.quit();
+
+    }
+    }
